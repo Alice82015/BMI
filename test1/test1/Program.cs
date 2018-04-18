613a9674cd1ef9561f1b8b2d9d7fbe179e6067e2
DESKTOP-LTLCP32\GC17 <a106082015@mail.shu.edu.tw>
2018-04-18 22:30:16 +0800

    增加註解

--- test1/test1/Program.cs
+++ test1/test1/Program.cs
@@ -10,23 +10,27 @@ namespace test1
     {
         static void Main(string[] args)
         {
+            // 增加介面與文字色彩
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Green;
 
+            // 輸入並讀取身高，轉字串為可計算數值
             Console.WriteLine("請輸入身高");
             string heigh = Console.ReadLine();
             double Heigh = double.Parse(heigh);
 
+            // 輸入並讀取體重，轉字串為可計算數值
             Console.WriteLine("請輸入體重");
             string weight = Console.ReadLine();
             double Weight = double.Parse(weight);
 
+            // 計算BMI
             double bmi = (Weight / ((Heigh / 100) * (Heigh / 100)));
 
-
+            // 顯示結果
             Console.WriteLine("你的BMI為" + bmi.ToString());
 
-
+            // 判斷與顯示其他結果
             if (bmi < 18.5)
             {
                 Console.WriteLine("你過輕了");
@@ -51,8 +55,12 @@ namespace test1
                     Console.WriteLine("要服役喔");
 
                 }
-                Console.ReadLine();
+
             }
+
+            // 暫停螢幕
+            Console.ReadLine();
+            
         }
     }
 }
